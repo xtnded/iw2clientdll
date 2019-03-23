8180a6b4d933807285b642e27302f88af5b37f88
kungfooman <lama12345@gmail.com>
2019-03-23 03:01:01 +0100

    %username% for postevent.bat

--- postevent.bat
+++ postevent.bat
@@ -1,3 +1,12 @@
-cd Release
-upx.exe mss32.dll
-cp mss32.dll "C:\Program Files (x86)\Steam\steamapps\common\Call of Duty 2\mss32.dll"
+@echo off
+
+if %username% == php (
+	cd Release
+	upx.exe mss32.dll
+	cp mss32.dll "C:\Program Files (x86)\Steam\steamapps\common\Call of Duty 2\mss32.dll"
+)
+
+if %username% == kung (
+	cd Release
+	copy mss32.dll C:\games\cod2_1_3_clean_for_mapping
+)
