87dd191a2dbea09fa3abf377d3129aba9097285e
PrawyCoD1 <cod1prawy@gmail.com>
2023-04-16 01:12:59 +0200

    Update .gitignore + fix

--- patch_gfx_dll.cpp
+++ patch_gfx_dll.cpp
@@ -689,7 +689,7 @@ void patch_gfx_dll()
 	*/
 	//extern char sys_cmdline[1024];
 	cvar_t* r_windowed = Cvar_RegisterBool("r_windowed", "0", CVAR_ARCHIVE);
-	if (r_windowed->integer == 1) {
+	if (r_windowed->integer) {
 		XUNLOCK((void*)GFX_OFF(0x10011564), 1);
 		XUNLOCK((void*)GFX_OFF(0x10012A8A), 1);
 		*(unsigned char*)GFX_OFF(0x10011564) = 0xeb;
