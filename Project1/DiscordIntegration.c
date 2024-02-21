20bf9a173febc8e2da3364ea1d7b8280e88a409e
Prawy <cod1prawy@gmail.com>
2024-02-21 02:38:08 +0100

    fixed discord rpc status
    Now it contains number of players ex (5 of 64) like it was before it was broken

--- Project1/DiscordIntegration.c
+++ Project1/DiscordIntegration.c
@@ -10,15 +10,14 @@
 #define MSG_COLS 7
 
 short int convtl_prcd(
-	char *str_ptr,
-	long *result
+	char* str_ptr,
+	long* result
 ) {
-	char *ptr;
-	long num = strtol(str_ptr, &ptr, 10);
+	char* ptr;
+	*result = strtol(str_ptr, &ptr, 10);
 	if (ptr == str_ptr) {
 		return -1;
 	}
-	result = &num;
 	return 0;
 }
 
@@ -28,30 +27,23 @@ int main(void) {
 	DiscordEventHandlers handlers;
 	memset(&handlers, 0x0, sizeof(handlers));
 	Discord_Initialize("1109509865279332392", &handlers, 1, NULL);
-	
+
 	short int readed_bytes;
-	
-	char *conv_ptr = NULL;
+
+	char* conv_ptr = NULL;
 	char buff[BUFF_SIZE];
 	char msg[MSG_COLS][COL_SIZE] = { 0 };
-	
-	long* party_size = NULL, * party_max = NULL;
+
+	long party_size = 0, party_max = 0;
 	DiscordRichPresence presence;
 
-	while (
-		(readed_bytes = _read(0, buff, BUFF_SIZE)) > 0
-	) {
+	while ((readed_bytes = _read(0, buff, BUFF_SIZE)) > 0) {
 		memset(&presence, 0, sizeof(presence));
 
 		buff[readed_bytes - 1] = '\0';
-		//printf("%s\n", buff);
 		unsigned short int sep_counter = 0, msg_index = 0;
 
-		for (
-			unsigned short int buff_index = 0;
-			buff_index < readed_bytes;
-			buff_index++
-		) {
+		for (unsigned short int buff_index = 0; buff_index < readed_bytes; buff_index++) {
 			if (buff[buff_index] == '\t') {
 				msg[sep_counter][msg_index] = '\0';
 				msg_index = 0; sep_counter++;
@@ -62,39 +54,30 @@ int main(void) {
 			msg_index++;
 		}
 
-		// if the msg has been corrupted -> continue 
 		if (sep_counter != 6) {
 			buff[0] = '\0';
 			continue;
 		}
 
 		msg[sep_counter][msg_index] = '\0';
-		
-		//for (int x = 0; x < 7; x++) {
-		//	printf("%d => %s\n", x, msg[x]);
-		//	// Na koñcu robi siê jakiœ chuj
-		//}
-		//printf("\n");
 
 		presence.state = msg[0];
 		presence.details = msg[1];
 		presence.largeImageText = msg[2];
 		presence.largeImageKey = msg[3];
 
-		if (convtl_prcd(msg[5], party_size) ||convtl_prcd(msg[6], party_max)
-		) {
-			goto send_data;
+		if (convtl_prcd(msg[5], &party_size) == 0 && convtl_prcd(msg[6], &party_max) == 0) {
+			if (party_size > 0 && party_size < 65) {
+				presence.partySize = party_size;
+				presence.partyMax = party_max;
+			}
 		}
 
-		if (0 < *party_size < 65) {
-			presence.partySize = party_size;
-			presence.partyMax = party_max;
-		}
-
-		send_data:
 		buff[0] = '\0';
 		Discord_UpdatePresence(&presence);
 	}
+
+	Discord_Shutdown();
 
 	return 0;
 }
